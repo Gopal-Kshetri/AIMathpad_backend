15d1308531be22ad24fe61a676b13acffe622e71
Orj <g.jivanphysics@gmail.com>
2023-02-06 22:25:23 +0545

    fixed the communication problem with front end

--- main.py
+++ main.py
@@ -3,16 +3,52 @@ from model_load import model_load
 from predict import make_prediction
 import numpy as np
 import cv2
+
+from pydantic import BaseModel
+
 app = FastAPI()
 
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
 
 app = FastAPI()
 
+
+origins = [
+    "http://localhost",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+import base64
+
+class Data(BaseModel):
+    img_file:str
+
 @app.post("/process-image")
-async def process_image(file: UploadFile):
-    image = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), -1)
+async def process_image(inp: Data):
+    img_64 = inp.img_file
+    # with open("imageToSave.jpg", "wb") as fh:
+    #     fh.write(base64.decodebytes(img_64.encode()))
+    nparr = np.fromstring(base64.b64decode(img_64), np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
+    black_mask = image[:,:,-1]>0
+
+    image[black_mask] = [255,255,255,255]
+    
+    cv2.imwrite('hellohi.jpg',image)
+    
+
     result = make_prediction(image)
+    print(result)
     return {"message": result}
